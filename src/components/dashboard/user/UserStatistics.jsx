36e87f2643279b090519694fb844a3088cdcd825
snokeOver <snokeover@gmail.com>
2024-09-08 15:31:14 +0600

    Added the user statistics page with custom bar chart

--- src/components/dashboard/user/UserStatistics.jsx
+++ src/components/dashboard/user/UserStatistics.jsx
@@ -1,5 +1,25 @@
+import useGetData from "../../../hooks/useGetData";
+import ChartView from "../shared/ChartView";
+import InitialPageStructure from "../shared/InitialPageStructure";
+
 const UserStatistics = () => {
-  return <div>User statistics comming soon</div>;
+  const {
+    data: userStatistics,
+    isPending,
+    error,
+  } = useGetData({ apiRoute: "user-statistics" });
+
+  return (
+    <InitialPageStructure
+      pageName="Dashboard"
+      pageTitle="User Statistics"
+      error={error}
+      isPending={isPending}
+      emptyDataMsg="Your chart is empty!"
+    >
+      <ChartView userStatistics={userStatistics} />
+    </InitialPageStructure>
+  );
 };
 
 export default UserStatistics;
